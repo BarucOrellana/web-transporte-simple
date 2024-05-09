90773b79ceda77a6ade441861328cc7047b87d8b
baruc <barucorellana@gmail.com>
2024-05-09 17:03:53 -0600

    change separate logic

---
+++ JS/home-page-seller.js
@@ -0,0 +1,37 @@
+        
+        let btn = document.getElementById("login-btn");
+        btn.addEventListener("click", login);
+
+    
+        const idSeller = localStorage.getItem("idSeller")
+        const jwt = localStorage.getItem("jwt")
+
+        async function login(event) {
+            event.preventDefault();
+    
+            let campos = {
+                destination: document.getElementById("destination").value,
+                date: document.getElementById("date").value,
+                description: document.getElementById("description").value,
+                idCapacity: parseInt(document.getElementById("capacity").value),
+                idProduct: parseInt(document.getElementById("product").value),
+                idSeller: parseInt(idSeller)
+            };
+    
+            const request = await fetch("http://localhost:8090/transporte_simple/freights/new-freight", {
+                method: 'POST', 
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + jwt
+                },
+                body: JSON.stringify(campos),
+                credentials: 'same-origin'
+            });
+            if (request.ok) {
+                window.location.href = "http://localhost:5500/HTML/home-page-seller.html";
+                alert("Viaje agregado");
+            } else {
+                console.error("Failed to authenticate");
+            }
+        }
